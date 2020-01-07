2bcd4e1dd2c8f03c17e4539bdd2479ac94cb2ffb
ryan-turnerNZ <ryanturnernz@gmail.com>
2020-01-07 14:33:15 +1300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -6,13 +6,13 @@ import '@firebase/firestore';
  * database
  */
 let app = firebase.initializeApp({
-  apiKey: 'AIzaSyB2urydpw_spNJ_B8C4iKuKS581yCpNxC4',
+  apiKey: 'API Key Here',
   authDomain: 'swen325-project2-d8610.firebaseapp.com',
   databaseURL: 'https://swen325-project2-d8610.firebaseio.com',
   projectId: 'swen325-project2-d8610',
   storageBucket: '',
   messagingSenderId: '747498989539',
-  appId: '1:747498989539:web:6c32b78965b41f441a62d6',
+  appId: 'App Id Here',
 });
 
 export const db = app.firestore();
